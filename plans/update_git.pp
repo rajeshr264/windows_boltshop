f1f61ec013ec86aa06367ab2288db7144cdd6b18
Rajesh Radhakrishnan <4780454+rajeshr264@users.noreply.github.com>
2023-02-22 11:16:31 -0800

    Update update_git.pp

--- plans/update_git.pp
+++ plans/update_git.pp
@@ -5,5 +5,5 @@ plan windows_boltshop::update_git (
   # Use puppetdb to find the nodes from the “other” team's web cluster
   $query = [from, nodes, ['=', [fact, env], $environment]]
   $selected_nodes = puppetdb_query($query).map() |$target| { $target[certname] }
-  run_task('puppet_agent::version', $selected_nodes)
+  run_task('windows_boltshop::helloworld', $selected_nodes, name => "hello")
 }
