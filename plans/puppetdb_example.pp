c4c947329b55a09e8a4e5a2666a6bc7641c0c0b1
Rajesh Radhakrishnan <4780454+rajeshr264@users.noreply.github.com>
2023-02-24 10:27:01 -0800

    Update puppetdb_example.pp

--- plans/puppetdb_example.pp
+++ plans/puppetdb_example.pp
@@ -1,7 +1,8 @@
 plan windows_boltshop::puppetdb_example (
   Enum['dev', 'prod'] $environment,
 ) {
-  # Use puppetdb to find the nodes from the “other” team's web cluster
+
+  # query puppetdb to find the nodes that have the 'dev' or 'prod' fact  
   $query = [from, nodes, ['=', [fact, env], $environment]]
   $selected_nodes = puppetdb_query($query).map() |$target| { $target[certname] }
   run_task('windows_boltshop::helloworld', $selected_nodes, name => "hello")
