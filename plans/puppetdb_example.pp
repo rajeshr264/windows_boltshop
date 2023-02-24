6a923a8209dd857e0d1ab636a80160267ec43635
Rajesh Radhakrishnan <4780454+rajeshr264@users.noreply.github.com>
2023-02-24 10:29:46 -0800

    Update puppetdb_example.pp

--- plans/puppetdb_example.pp
+++ plans/puppetdb_example.pp
@@ -1,9 +1,12 @@
 plan windows_boltshop::puppetdb_example (
   Enum['dev', 'prod'] $environment,
 ) {
-
-  # query puppetdb to find the nodes that have the 'dev' or 'prod' fact  
-  $query = [from, nodes, ['=', [fact, env], $environment]]
+  
+  # Assumption: There are target nodes managed by Puppet, that have a fact called 'env' installed on them. 
+  # Some nodes have values 'env = dev' & some have 'env = prod'.
+  
+  # query puppetdb to find the nodes that have the 'env = dev' or 'env = prod' fact  
+  $query = [from, nodes, ['=', [fact, env], $environment]]. # note that the fact name 'env' is used in the query string
   $selected_nodes = puppetdb_query($query).map() |$target| { $target[certname] }
   run_task('windows_boltshop::helloworld', $selected_nodes, name => "hello")
 }
